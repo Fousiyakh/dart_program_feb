a7b09680d5ea0064bd2b101ac89834f4c42a5dd2
Fousiyakh <fousiyakh12@gmail.com>
2024-02-26 10:36:01 +0530

    Initial commit

---
+++ bin/sample.dart
@@ -0,0 +1,16 @@
+void main(){
+  String name = "Fousiya K H";
+  int age = 21;
+  String emailid ="fousiykh12@gmail.com";
+  double cgpa = 7.8;
+  print("Personal Details ");
+  print("Name : $name");
+  print("Age : $age");
+  print("Email Id :$emailid");
+  print("CGPA :$cgpa");
+
+
+
+
+
+}
